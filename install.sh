0ad34359db8e55c59fa699c3b72607314abcd3e2
Giuseppe Lumia <g.lumia@outlook.com>
2021-04-03 10:46:30 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 LOCAL_BIN="$HOME/.local/bin"
 PWD=$(pwd)
 
-echo "Installing custom scripts and executables into ~/.local/bin"
+printf "Installing custom scripts and executables into ~/.local/bin\n"
 mkdir -p "$LOCAL_BIN"
 for file in bin/*; do
-	ln -s "$PWD/$file" "$LOCAL_BIN/$(basename "$file")"
+	printf "\tlinking ~/.local/$file to ./$file\n"
+	ln -sf "$PWD/$file" "$LOCAL_BIN/$(basename "$file")"
 done
 
 
-echo "Linking dotfiles to home"
+printf "\nLinking dotfiles to home\n"
 DOTFILES="
 	.bash_aliases
 	.gitconfig
@@ -18,5 +19,6 @@ DOTFILES="
 	.vimrc
 "
 for file in $DOTFILES; do
-	ln -s "$PWD/$file" "$HOME/$file"
+	printf "\tlinking ~/$file to ./$file\n"
+	ln -sf "$PWD/$file" "$HOME/$file"
 done
