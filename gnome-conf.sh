8d64493dae6b8de4ff5a119b5c30820df90e1481
Giuseppe Lumia <gius@glumia.dev>
2023-05-23 00:23:04 +0200

    Minimize Gnome customization

--- gnome-conf.sh
+++ gnome-conf.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 printf "Installing GNOME config\n"
-walldir="/home/$USER/.local/share/backgrounds"
-mkdir -p "$walldir"
-cp wallpapers/anna-wangler-turtle-unsplash.jpg "$walldir"
 dconf load / < dconf.txt
